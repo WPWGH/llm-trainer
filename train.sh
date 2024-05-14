3954514021bb4de15459d4963643801b43723999
WPWGH <116812664+WPWGH@users.noreply.github.com>
2024-05-14 21:25:14 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
-PORTS=$ARNOLD_WORKER_0_PORT
-PORT=(${PORTS//,/ })
 
-# 使用硬编码的端口号
-AVAILABLE_PORT=12345
+# Number of GPUs per GPU worker
+GPUS_PER_NODE=$(python -c 'import torch; print(torch.cuda.device_count())')
 
-torchrun --nproc_per_node $ARNOLD_WORKER_GPU \
-    --nnodes $ARNOLD_WORKER_NUM \
-    --node_rank=$ARNOLD_ID \
-    --master_addr $ARNOLD_WORKER_0_HOST \
-    --master_port $AVAILABLE_PORT src/train.py --params=$1
+# Number of GPU workers, for single-worker training, please set to 1
+NNODES=${NNODES:-1}
+
+# The rank of this worker, should be in {0, ..., WORKER_CNT-1}, for single-worker training, please set to 0
+NODE_RANK=${NODE_RANK:-0}
+
+# The ip address of the rank-0 worker, for single-worker training, please set to localhost
+MASTER_ADDR=${MASTER_ADDR:-localhost}
+
+# The port for communication
+MASTER_PORT=${MASTER_PORT:-6001}
+
+torchrun --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank=$NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT src/inference.py --params=$1
